b6854a7c813f3183326cea4157bf7b3294cd2452
Md. Aminul Islam Fahim <cipfahim1999@gmail.com>
2021-04-29 13:01:54 +0600

    new dashboard ui ready

---
+++ resources/js/Layouts/Partials/Sidebar.vue
@@ -0,0 +1,142 @@
+<template>
+    <TransitionRoot as="template" :show="sidebarOpen">
+        <Dialog as="div" static class="fixed inset-0 flex z-40 lg:hidden" @close="sidebarOpen = false"
+                :open="sidebarOpen">
+            <TransitionChild as="template" enter="transition-opacity ease-linear duration-300" enter-from="opacity-0"
+                             enter-to="opacity-100" leave="transition-opacity ease-linear duration-300"
+                             leave-from="opacity-100" leave-to="opacity-0">
+                <DialogOverlay class="fixed inset-0 bg-gray-600 bg-opacity-75"/>
+            </TransitionChild>
+            <TransitionChild as="template" enter="transition ease-in-out duration-300 transform"
+                             enter-from="-translate-x-full" enter-to="translate-x-0"
+                             leave="transition ease-in-out duration-300 transform" leave-from="translate-x-0"
+                             leave-to="-translate-x-full">
+                <div class="relative flex-1 flex flex-col max-w-xs w-full pt-5 pb-4 bg-cyan-700">
+                    <TransitionChild as="template" enter="ease-in-out duration-300" enter-from="opacity-0"
+                                     enter-to="opacity-100" leave="ease-in-out duration-300" leave-from="opacity-100"
+                                     leave-to="opacity-0">
+                        <div class="absolute top-0 right-0 -mr-12 pt-2">
+                            <button
+                                class="ml-1 flex items-center justify-center h-10 w-10 rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                                @click="sidebarOpen = false">
+                                <span class="sr-only">Close sidebar</span>
+                                <XIcon class="h-6 w-6 text-white" aria-hidden="true"/>
+                            </button>
+                        </div>
+                    </TransitionChild>
+                    <div class="flex-shrink-0 flex items-center px-4">
+                        <img class="h-8 w-auto"
+                             src="https://tailwindui.com/img/logos/easywire-logo-cyan-300-mark-white-text.svg"
+                             alt="Easywire logo"/>
+                    </div>
+                    <nav class="mt-5 flex-shrink-0 h-full divide-y divide-cyan-800 overflow-y-auto"
+                         aria-label="Sidebar">
+                        <div class="px-2 space-y-1">
+                            <a v-for="item in navigation" :key="item.name" :href="item.href"
+                               :class="[item.current ? 'bg-cyan-800 text-white' : 'text-cyan-100 hover:text-white hover:bg-cyan-600', 'group flex items-center px-2 py-2 text-base font-medium rounded-md']"
+                               :aria-current="item.current ? 'page' : undefined">
+                                <component :is="item.icon" class="mr-4 h-6 w-6 text-cyan-200" aria-hidden="true"/>
+                                {{ item.name }}
+                            </a>
+                        </div>
+                        <div class="mt-6 pt-6">
+                            <div class="px-2 space-y-1">
+                                <a v-for="item in secondaryNavigation" :key="item.name" :href="item.href"
+                                   class="group flex items-center px-2 py-2 text-base font-medium rounded-md text-cyan-100 hover:text-white hover:bg-cyan-600">
+                                    <component :is="item.icon" class="mr-4 h-6 w-6 text-cyan-200" aria-hidden="true"/>
+                                    {{ item.name }}
+                                </a>
+                            </div>
+                        </div>
+                    </nav>
+                </div>
+            </TransitionChild>
+            <div class="flex-shrink-0 w-14" aria-hidden="true">
+                <!-- Dummy element to force sidebar to shrink to fit close icon -->
+            </div>
+        </Dialog>
+    </TransitionRoot>
+
+    <!-- Static sidebar for desktop -->
+    <div class="hidden lg:flex lg:flex-shrink-0">
+        <div class="flex flex-col w-64">
+            <!-- Sidebar component, swap this element with another sidebar if you like -->
+            <div class="flex flex-col flex-grow bg-cyan-700 pt-5 pb-4 overflow-y-auto">
+                <div class="flex items-center flex-shrink-0 px-4">
+                    <img class="h-8 w-auto"
+                         src="https://tailwindui.com/img/logos/easywire-logo-cyan-300-mark-white-text.svg"
+                         alt="Easywire logo"/>
+                </div>
+                <nav class="mt-5 flex-1 flex flex-col divide-y divide-cyan-800 overflow-y-auto" aria-label="Sidebar">
+                    <div class="px-2 space-y-1">
+                        <a v-for="item in navigation" :key="item.name" :href="item.href"
+                           :class="[item.current ? 'bg-cyan-800 text-white' : 'text-cyan-100 hover:text-white hover:bg-cyan-600', 'group flex items-center px-2 py-2 text-sm leading-6 font-medium rounded-md']"
+                           :aria-current="item.current ? 'page' : undefined">
+                            <component :is="item.icon" class="mr-4 h-6 w-6 text-cyan-200" aria-hidden="true"/>
+                            {{ item.name }}
+                        </a>
+                    </div>
+                    <div class="mt-6 pt-6">
+                        <div class="px-2 space-y-1">
+                            <a v-for="item in secondaryNavigation" :key="item.name" :href="item.href"
+                               class="group flex items-center px-2 py-2 text-sm leading-6 font-medium rounded-md text-cyan-100 hover:text-white hover:bg-cyan-600">
+                                <component :is="item.icon" class="mr-4 h-6 w-6 text-cyan-200" aria-hidden="true"/>
+                                {{ item.name }}
+                            </a>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import {ref} from 'vue'
+import {Dialog, DialogOverlay, TransitionChild, TransitionRoot,} from '@headlessui/vue'
+import {
+    ClockIcon,
+    CogIcon,
+    CreditCardIcon,
+    DocumentReportIcon,
+    HomeIcon,
+    QuestionMarkCircleIcon,
+    ScaleIcon,
+    ShieldCheckIcon,
+    UserGroupIcon,
+    XIcon,
+} from '@heroicons/vue/outline'
+
+const navigation = [
+    {name: 'Home', href: '#', icon: HomeIcon, current: true},
+    {name: 'History', href: '#', icon: ClockIcon, current: false},
+    {name: 'Balances', href: '#', icon: ScaleIcon, current: false},
+    {name: 'Cards', href: '#', icon: CreditCardIcon, current: false},
+    {name: 'Recipients', href: '#', icon: UserGroupIcon, current: false},
+    {name: 'Reports', href: '#', icon: DocumentReportIcon, current: false},
+]
+const secondaryNavigation = [
+    {name: 'Settings', href: '#', icon: CogIcon},
+    {name: 'Help', href: '#', icon: QuestionMarkCircleIcon},
+    {name: 'Privacy', href: '#', icon: ShieldCheckIcon},
+]
+
+export default {
+    components: {
+        Dialog,
+        DialogOverlay,
+        TransitionRoot,
+        TransitionChild,
+        XIcon
+    },
+    setup() {
+        const sidebarOpen = ref(false)
+
+        return {
+            sidebarOpen,
+            navigation,
+            secondaryNavigation
+        }
+    },
+}
+</script>
