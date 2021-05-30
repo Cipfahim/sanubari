68997f1ff3796e85e45fd0a3321566792590b332
alamindevms <alamin.mistrisolutions@gmail.com>
2021-05-30 15:12:39 +0600

    contact details

--- resources/js/Pages/Employees/ContactDetails.vue
+++ resources/js/Pages/Employees/ContactDetails.vue
@@ -44,146 +44,197 @@
         ]"
       />
       <div class="bg-gray-50 p-4 rounded-md mt-5">
-        <!-- Tab for Mobile -->
-        <div class="sm:hidden">
-          <label for="tabs" class="sr-only">Select a tab</label>
-          <select
-            id="tabs"
-            name="tabs"
-            class="block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-cyan-500 focus:border-cyan-500 sm:text-sm rounded-md"
-          >
-            <option>Contact Number</option>
-
-            <option>Personal Email</option>
-
-            <option selected>Contact Address</option>
-          </select>
-        </div>
-        <!-- Tab for Desktop -->
-        <div class="hidden sm:block">
+        
+        <!-- Tab for Desktop & mobile -->
+        <div class="block sm:block overflow-x-scroll scrollbar-hide">
           <div class="border-b border-gray-200">
-            <nav class="-mb-px flex space-x-8" aria-label="Tabs">
+            <nav class="-mb-px flex space-x-6 sm:space-x-8" aria-label="Tabs">
               <!-- Current: "border-cyan-500 text-cyan-600", Default: "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-200" -->
-              <a
-                href="#"
-                
-                class="border-cyan-500 text-cyan-600 whitespace-nowrap flex py-4 px-1 border-b-2 font-medium text-sm"
+              <button
+                type="button"
+                @click="toggleTabs(1)"
+                :class="{
+                  'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-200':
+                    openTab !== 1,
+                  'border-cyan-500 text-cyan-600': openTab === 1,
+                }"
+                class="whitespace-nowrap flex py-4 px-1 border-b-2 font-medium text-sm focus:outline-none"
               >
                 Contact Number
 
                 <!-- Current: "bg-cyan-100 text-cyan-600", Default: "bg-gray-100 text-gray-900" -->
                 <span
-                  class="bg-cyan-100 text-cyan-600 hidden ml-3 py-0.5 px-2.5 rounded-full text-xs font-medium md:inline-block"
+                  class="hidden ml-3 py-0.5 px-2.5 rounded-full text-xs font-medium md:inline-block"
+                  :class="{
+                    'bg-gray-100 text-gray-900 ': openTab !== 1,
+                    'bg-cyan-100 text-cyan-600 ': openTab === 1,
+                  }"
                   >2</span
                 >
-              </a>
+              </button>
 
-              <a
-                href="#"
-                class="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-200 whitespace-nowrap flex py-4 px-1 border-b-2 font-medium text-sm"
+              <button
+                type="button"
+                v-on:click="toggleTabs(2)"
+                v-bind:class="{
+                  'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-200':
+                    openTab !== 2,
+                  'border-cyan-500 text-cyan-600': openTab === 2,
+                }"
+                class="whitespace-nowrap flex py-4 px-1 border-b-2 font-medium text-sm focus:outline-none"
               >
                 Personal Email
 
                 <span
-                  class="bg-gray-100 text-gray-900 hidden ml-3 py-0.5 px-2.5 rounded-full text-xs font-medium md:inline-block"
+                  class="hidden ml-3 py-0.5 px-2.5 rounded-full text-xs font-medium md:inline-block"
+                  :class="{
+                    'bg-gray-100 text-gray-900 ': openTab !== 2,
+                    'bg-cyan-100 text-cyan-600 ': openTab === 2,
+                  }"
                   >3</span
                 >
-              </a>
+              </button>
 
-              <a
-                href="#"
-                class="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-200 whitespace-nowrap flex py-4 px-1 border-b-2 font-medium text-sm"
+              <button
+                type="button"
+                v-on:click="toggleTabs(3)"
+                v-bind:class="{
+                  'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-200':
+                    openTab !== 3,
+                  'border-cyan-500 text-cyan-600': openTab === 3,
+                }"
+                class="whitespace-nowrap flex py-4 px-1 border-b-2 font-medium text-sm focus:outline-none"
                 aria-current="page"
               >
                 Contact Address
 
-                
                 <span
-                  class="bg-gray-100 text-gray-900 hidden ml-3 py-0.5 px-2.5 rounded-full text-xs font-medium md:inline-block"
+                  class="hidden ml-3 py-0.5 px-2.5 rounded-full text-xs font-medium md:inline-block"
+                  :class="{
+                    'bg-gray-100 text-gray-900 ': openTab !== 3,
+                    'bg-cyan-100 text-cyan-600 ': openTab === 3,
+                  }"
                   >5</span
                 >
-              </a>
+              </button>
             </nav>
           </div>
         </div>
         <!-- Personal Phone -->
-        <div class="mt-4">
-          <div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:pt-2">
-            <label
-              class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
-            >
-              Contact Number<span class="text-blue-600"> *</span>
-            </label>
+        <div
+          class="mt-4 h-full sm:h-96"
+          v-bind:class="{ hidden: openTab !== 1, block: openTab === 1 }"
+        >
+          <!-- <div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:pt-2"> -->
+          <div class="w-full sm:w-3/6 mx-auto sm:pt-2 sm:px-3">
+            <div class="flex items-center justify-between mb-3">
+              <label class="text-base font-medium text-gray-700 sm:mt-px sm:pt-2">
+                Contact Number<span class="text-blue-600"> *</span>
+              </label>
+              
+              <button
+                  type="button"
+                  class="bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500 flex items-center"
+                >
+                  <PlusCircleIcon class="h-5 w-5 text-cyan-400 mr-2" />
+                  Add
+                </button>
+            </div>
 
-            <div class="mt-1 sm:mt-0 sm:col-span-2">
-              <div class="max-w-lg flex justify-space-between">
+            <div class="mt-1 sm:mt-0">
+              <vue-tel-input
+                  v-model="phone"
+                  mode="international"
+                  class="focus:ring-cyan-500 focus:border-cyan-500 block w-full sm:text-sm border border-gray-300 rounded-md mb-3"
+              >
+              </vue-tel-input>
+
+              <div class="relative">
                 <vue-tel-input
                   v-model="phone"
                   mode="international"
-                  class="focus:ring-cyan-500 focus:border-cyan-500 block w-full sm:text-sm border border-gray-300 rounded-md"
+                  class="focus:ring-cyan-500 focus:border-cyan-500 block w-full sm:text-sm border border-gray-300 rounded-md mb-3"
                 >
                 </vue-tel-input>
-
-                <button
-                  type="button"
-                  class="ml-2 bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500 flex items-center"
-                >
-                  <TrashIcon class="h-5 w-5 text-red-400 mr-2" />
-                  Delete
+                <button class="absolute right-2 top-2 text-red-500 hover:bg-gray-200 rounded-full h-7 w-7 text-center">
+                  <XIcon class="h-5 w-5 text-red-400 mx-auto" />
                 </button>
               </div>
             </div>
           </div>
         </div>
         <!-- Personal Email -->
-        <div class="mt-4">
-          <div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:pt-2">
-            <label
-              class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
-            >
-              Contact Email<span class="text-blue-600"> *</span>
-            </label>
-
-            <div class="mt-1 sm:mt-0 sm:col-span-2">
-              <div class="max-w-lg flex justify-space-between">
-                <input
-                  type="email"
-                  class="max-w-lg block w-full shadow-sm focus:ring-cyan-500 focus:border-cyan-500 sm:max-w-lg sm:text-sm border-gray-300 rounded-md"
-                />
-
-                <button
+        <div
+          class="mt-4 h-full sm:h-96"
+          v-bind:class="{ hidden: openTab !== 2, block: openTab === 2 }"
+        > 
+          <div class="w-full sm:w-3/6 mx-auto sm:pt-2 sm:px-3">
+            <div class="flex items-center justify-between mb-3">
+              <label class="text-base font-medium text-gray-700 sm:mt-px sm:pt-2">
+                Contact Email<span class="text-blue-600"> *</span>
+              </label>
+              
+              <button
                   type="button"
-                  class="ml-2 bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500 flex items-center"
+                  class="bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500 flex items-center"
                 >
                   <PlusCircleIcon class="h-5 w-5 text-cyan-400 mr-2" />
                   Add
                 </button>
+            </div>
+
+            <div class="mt-1 sm:mt-0">
+              <input
+                  type="email"
+                  class="max-w-lg block w-full shadow-sm focus:ring-cyan-500 focus:border-cyan-500 sm:max-w-lg sm:text-sm border-gray-300 rounded-md mb-3"
+                />
+
+              <div class="relative">
+                <input
+                  type="email"
+                  class="max-w-lg block w-full shadow-sm focus:ring-cyan-500 focus:border-cyan-500 sm:max-w-lg sm:text-sm border-gray-300 rounded-md mb-3"
+                />
+                <button class="absolute right-2 top-1.5 text-red-500 hover:bg-gray-200 rounded-full h-7 w-7 text-center">
+                  <XIcon class="h-5 w-5 text-red-400 mx-auto" />
+                </button>
               </div>
             </div>
           </div>
+          
         </div>
         <!-- Address -->
-        <div class="mt-4">
-          <div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:pt-2">
-            <label
-              class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
-            >
-              Contact Address<span class="text-blue-600"> *</span>
-            </label>
-
-            <div class="mt-1 sm:mt-0 sm:col-span-2">
-              <div class="max-w-lg flex justify-space-between">
-                <input
-                  type="email"
-                  class="max-w-lg block w-full shadow-sm focus:ring-cyan-500 focus:border-cyan-500 sm:max-w-lg sm:text-sm border-gray-300 rounded-md"
-                />
-
-                <button
+        <div
+          class="mt-4 h-full sm:h-96"
+          v-bind:class="{ hidden: openTab !== 3, block: openTab === 3 }"
+        > 
+          <div class="w-full sm:w-3/6 mx-auto sm:pt-2 sm:px-3">
+            <div class="flex items-center justify-between mb-3">
+              <label class="text-base font-medium text-gray-700 sm:mt-px sm:pt-2">
+                Contact Address<span class="text-blue-600"> *</span>
+              </label>
+              
+              <button
                   type="button"
-                  class="ml-2 bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500 flex items-center"
+                  class="bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500 flex items-center"
                 >
                   <PlusCircleIcon class="h-5 w-5 text-cyan-400 mr-2" />
                   Add
+                </button>
+            </div>
+
+            <div class="mt-1 sm:mt-0">
+              <input
+                  type="email"
+                  class="max-w-lg block w-full shadow-sm focus:ring-cyan-500 focus:border-cyan-500 sm:max-w-lg sm:text-sm border-gray-300 rounded-md mb-3"
+                />
+
+              <div class="relative">
+                <input
+                  type="email"
+                  class="max-w-lg block w-full shadow-sm focus:ring-cyan-500 focus:border-cyan-500 sm:max-w-lg sm:text-sm border-gray-300 rounded-md mb-3"
+                />
+                <button class="absolute right-2 top-1.5 text-red-500 hover:bg-gray-200 rounded-full h-7 w-7 text-center">
+                  <XIcon class="h-5 w-5 text-red-400 mx-auto" />
                 </button>
               </div>
             </div>
@@ -198,6 +249,7 @@
 <script>
 import AppLayout from "../../Layouts/App";
 import TopBar from "./TopBar";
+
 import {
   CalendarIcon,
   CashIcon,
@@ -206,7 +258,7 @@ import {
   ChevronRightIcon,
   OfficeBuildingIcon,
   PlusCircleIcon,
-  TrashIcon,
+  XIcon,
 } from "@heroicons/vue/solid";
 import { mask } from "vue-the-mask";
 import JetFormSection from "@/Jetstream/FormSection";
@@ -237,7 +289,7 @@ export default {
     CheckIcon,
     CalendarIcon,
     PlusCircleIcon,
-    TrashIcon,
+    XIcon,
     JetFormSection,
     JetSecondaryButton,
     JetLabel,
@@ -261,11 +313,15 @@ export default {
           resetOnSuccess: true,
         }
       ),
+      openTab: 1,
     };
   },
   methods: {
     submit() {
       this.form.post(this.route("employees.update", this.employee.id));
+    },
+    toggleTabs: function (tabNumber) {
+      this.openTab = tabNumber;
     },
   },
 };
