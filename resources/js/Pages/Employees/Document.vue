3247c0cc058b9d9bf77177e7b1a25ef8cb9957be
alamindevms <alamin.mistrisolutions@gmail.com>
2021-06-01 16:49:53 +0600

    view page

---
+++ resources/js/Pages/Employees/Document.vue
@@ -0,0 +1,130 @@
+<template>
+    <app-layout>
+        <!-- Start::Stepper -->
+        <div class="mt-4 px-4 sm:px-6 lg:px-8">
+            <div class="w-full flex justify-between items-center mb-4">
+                <h1 class="text-xl font-bold text-gray-800">{{ employee.official_name }}</h1>
+                <inertia-link
+                    href="#"
+                    class="py-2 px-4 border border-transparent font-bold shadow-sm text-sm rounded-md text-white bg-gray-700 hover:bg-gray-800 focus:outline-none"
+                >
+                    Add Document
+                </inertia-link>
+            </div>
+            <!-- This example requires Tailwind CSS v2.0+ -->
+            <div class="flex flex-col">
+                <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                    <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                        <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                            <table class="min-w-full divide-y divide-gray-200">
+                                <thead class="bg-gray-50">
+                                    <tr>
+                                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                            ID
+                                        </th>
+                                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                            File Description
+                                        </th>
+                                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                            Year
+                                        </th>
+                                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                            View
+                                        </th>
+                                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                            Tools
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody class="bg-white divide-y divide-gray-200">
+                                    <tr>
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            <div class="text-sm text-gray-900">23456</div>
+                                        </td>
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            <div class="text-sm text-gray-900">Medical Report</div>
+                                        </td>
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            <div class="text-sm text-gray-900">2017</div>
+                                        </td>
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            <img class="h-24 w-36 rounded" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixqx=GyX6XNR4mP&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60" alt="">
+                                        </td>
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            <div class="text-sm text-gray-900">Regional Paradigm Technician</div>
+                                            <div class="text-sm text-gray-500">Optimization</div>
+                                        </td>
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                                            Active
+                                            </span>
+                                        </td>
+                                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                            Admin
+                                        </td>
+                                        <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                            <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
+                                        </td>
+                                    </tr>
+                                    <!-- More people... -->
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- End::Stepper -->
+    </app-layout>
+</template>
+
+<script>
+import AppLayout from "../../Layouts/App";
+import TopBar from "./TopBar";
+import {
+    CalendarIcon,
+    CashIcon,
+    CheckCircleIcon,
+    CheckIcon,
+    ChevronRightIcon,
+    OfficeBuildingIcon,
+    PlusCircleIcon,
+} from "@heroicons/vue/solid";
+import JetFormSection from "@/Jetstream/FormSection";
+import JetLabel from "@/Jetstream/Label";
+import JetInputError from "@/Jetstream/InputError";
+import JetInput from "@/Jetstream/Input";
+import JetActionMessage from "@/Jetstream/ActionMessage";
+import JetButton from "@/Jetstream/Button";
+import JetSecondaryButton from "@/Jetstream/SecondaryButton";
+import Input from "../../Components/Input";
+import JetCheckbox from "@/Jetstream/Checkbox";
+import {Switch} from "@headlessui/vue";
+
+export default {
+    props: {
+        employee: Object,
+    },
+    components: {
+        Input,
+        TopBar,
+        AppLayout,
+        OfficeBuildingIcon,
+        CheckCircleIcon,
+        CashIcon,
+        ChevronRightIcon,
+        CheckIcon,
+        CalendarIcon,
+        PlusCircleIcon,
+        JetFormSection,
+        JetSecondaryButton,
+        JetLabel,
+        JetInput,
+        JetInputError,
+        JetActionMessage,
+        JetButton,
+        JetCheckbox,
+        Switch
+    },
+};
+</script>
