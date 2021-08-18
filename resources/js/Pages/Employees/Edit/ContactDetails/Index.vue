d833185c952c2c1a07c4fee81a2240bb7b7ce54e
Al Amin <alamin.mistrisolutions@gmail.com>
2021-08-18 10:22:28 +0600

    contact details tab

--- resources/js/Pages/Employees/Edit/ContactDetails/Index.vue
+++ resources/js/Pages/Employees/Edit/ContactDetails/Index.vue
@@ -8,7 +8,7 @@
                 <!-- Tab for Desktop & mobile -->
                 <div class="block sm:block overflow-x-scroll scrollbar-hide text-center">
                     <div class="border-b border-gray-200">
-                        <nav class="-mb-px flex space-x-6 sm:space-x-8" aria-label="Tabs">
+                        <nav class="-mb-px flex justify-center space-x-6 sm:space-x-8" aria-label="Tabs">
                             <!-- Current: "border-cyan-500 text-cyan-600", Default: "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-200" -->
                             <button
                                 type="button"
