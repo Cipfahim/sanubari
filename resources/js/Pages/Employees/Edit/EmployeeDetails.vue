0f71efb27c6efb8573b220d49ddb139fa4b8bcc6
Md.Aminul Islam Fahim <cipfahim1999@gmail.com>
2021-08-21 10:00:12 +0600

    jani na ke korse

--- resources/js/Pages/Employees/Edit/EmployeeDetails.vue
+++ resources/js/Pages/Employees/Edit/EmployeeDetails.vue
@@ -110,13 +110,13 @@
                                                     </option>
                                                 </select>
 
-                                                <button
-                                                    type="button"
+                                                <inertia-link
+                                                    :href="route('locations.create')"
                                                     class="ml-2 bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500 flex items-center"
                                                 >
                                                     <PlusCircleIcon class="h-5 w-5 text-cyan-400 mr-2"/>
                                                     Add
-                                                </button>
+                                                </inertia-link>
 
                                             </div>
                                             <jet-input-error
